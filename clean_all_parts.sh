e8abe16cdb22fbea1b234c27e0a63bad2bfdb034
MohSeyam <mohammedsiam15@gmail.com>
2025-07-17 18:06:13 +0300

    Add/Update plan data and clean up unnecessary files

--- clean_all_parts.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for i in {1..8}
-do
-  cp 2_$i.md 2.md
-  node deepCleanPlanData.js
-  mv 2_clean.json 2_clean_$i.json
-done
-echo "تم تنظيف جميع الأجزاء وحفظها في 2_clean_1.json ... 2_clean_8.json"
