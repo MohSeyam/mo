9ac0d63d5049e335403f984ecbfd7ed1b860f57e
Moh Seyam <116368645+MohSeyam@users.noreply.github.com>
2025-07-18 10:15:16 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import { ToastProvider } from './context/ToastContext';
 import { AppProvider } from './context/AppContext';
-import { planData } from './components/App';
+// import { planData } from './components/App';
+import planData from '../data/planData.json';
 
 ReactDOM.render(
   <ToastProvider>
